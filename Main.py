6617b18a8d2c5f653d49ec29d874fc84e6f3ee60
Burak Enes BEYGO <burakebeygo@gmail.com>
2023-12-02 21:57:25 +0300

    csv data imported

--- Main.py
+++ Main.py
@@ -1,8 +1,20 @@
 import numpy as np
 import pandas as pd
 import csv
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import sent_tokenize, word_tokenize
 
 with open('dblp-v10_sample.csv', newline='') as csvfile:
-    myreader = csv.reader(csvfile, delimiter=',', quotechar='|')
-    for row in myreader:
-        print(', '.join(row))
+    mycsvreader = csv.reader(csvfile, delimiter=',', quotechar='"')
+    for row in mycsvreader:
+        abstract = row[0]
+        authors = row[1]
+        n_citation = row[2]
+        references = row[3]
+        title = row[4]
+        venue = row[5]
+        year = row[6]
+        id = row[7]
+        word = nltk.word_tokenize(id)
+        print(word)
