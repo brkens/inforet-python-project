92130d3e5aad970147c107884aeccdd7d6573573
Burak Enes BEYGO <burakebeygo@gmail.com>
2023-12-09 18:26:08 +0300

    boolean retrieval done

--- Main.py
+++ Main.py
@@ -9,7 +9,15 @@ from nltk.corpus import stopwords
 stemmed_documents = []
 lemmatized_documents = []
 
-with open('dblp-v10_sample.csv', newline='') as csvfile:
+query_sentences = []
+stemmed_query_words = []
+lemmatized_query_words = []
+
+stop_words = set(stopwords.words("english"))
+stemmer = PorterStemmer()
+lemmatizer = WordNetLemmatizer()
+
+with open('dblp-v10.csv', newline='', encoding="utf8") as csvfile:
     mycsvreader = csv.reader(csvfile, delimiter=',', quotechar='"')
     for row in mycsvreader:
         words = nltk.word_tokenize(row[0]) # abstract
@@ -21,21 +29,62 @@ with open('dblp-v10_sample.csv', newline='') as csvfile:
         words += nltk.word_tokenize(row[6]) # year
         words += nltk.word_tokenize(row[7]) # id
 
-        stop_words = set(stopwords.words("english"))
         filtered_words = []
         for word in words:
             if word.casefold() not in stop_words:
                 filtered_words.append(word)
 
         # Stemming
-        stemmer = PorterStemmer()
-        stemmed_words = [stemmer.stem(word) for word in filtered_words]
+        stemmed_words = [stemmer.stem(w) for w in filtered_words]
         stemmed_words_sentence = nltk.Text(stemmed_words)
         stemmed_documents.append(stemmed_words_sentence)
 
         # Lemmatization
-        lemmatizer = WordNetLemmatizer()
-        lemmatized_words = [lemmatizer.lemmatize(word.casefold()) for word in filtered_words]
+        lemmatized_words = [lemmatizer.lemmatize(w.casefold()) for w in filtered_words]
         lemmatized_words_sentence = nltk.Text(lemmatized_words)
         lemmatized_documents.append(lemmatized_words_sentence)
+
+with open('queries.txt', newline='') as f:
+    for line in f:
+        query_sentences.append(line.strip())
+
+for query in query_sentences:
+    query_words = nltk.word_tokenize(query)
+
+    filtered_query_words = []
+    for query_word in query_words:
+        if query_word.casefold() not in stop_words:
+            filtered_query_words.append(query_word)
+    
+    ############# Stemming ############# 
+    stemmed_query_words = [stemmer.stem(w) for w in filtered_query_words]
+
+    found_docs_stemmed = []
+    for i in range(len(stemmed_documents)):
+        match = False
+        for stemmed_query_word in stemmed_query_words:
+            if stemmed_query_word in stemmed_documents[i]:
+                match = True
+                break
+        
+        if match:
+            found_docs_stemmed.append(i + 1)
+    
+    print("STEMMED => Query", query_words, "found in", len(found_docs_stemmed), "documents. ") #They are", found_docs_stemmed)
+    
+    ############# Lemmatization #############
+    lemmatized_query_words = [lemmatizer.lemmatize(w.casefold()) for w in filtered_query_words]
+
+    found_docs_lemmatized = []
+    for i in range(len(lemmatized_documents)):
+        match = False
+        for lemmatized_query_word in lemmatized_query_words:
+            if lemmatized_query_word in lemmatized_documents[i]:
+                match = True
+                break
+        
+        if match:
+            found_docs_lemmatized.append(i + 1)
+    
+    print("LEMMATIZED => Query", query_words, "found in", len(found_docs_lemmatized), "documents. ") #They are", found_docs_lemmatized)
 
